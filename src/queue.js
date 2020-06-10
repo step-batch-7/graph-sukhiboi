f8f0368454c1f7770edc0a02a3dd4424247b6b75
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-06-10 21:00:02 +0530

    created queue

---
+++ src/queue.js
@@ -0,0 +1,15 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(element) {
+    this.queue.push(element);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+}
+
+module.exports = Queue;
