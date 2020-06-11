01dca1e23b1db12a244b8d5639fca47b3496ea70
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-06-11 10:38:26 +0530

    updated bfs function

--- src/graph.js
+++ src/graph.js
@@ -1,15 +1,26 @@
-const getChildren = function (pairs, node) {
-  return pairs.filter(([from]) => from === node).map(([, to]) => to);
+const getChildren = function (pairs, [source]) {
+  return pairs.filter(([from]) => from === source).map(([, to]) => to);
+};
+
+const parse = function (pairs) {
+  return pairs.reduce((graph, pair) => {
+    const [from] = pair;
+    if (graph[from] == undefined) {
+      graph[from] = getChildren(pairs, pair);
+    }
+    return graph;
+  }, {});
 };
 
 const bfs = function (pairs, source, target) {
   const visited = new Set();
   const to_visit = [source];
+  const graph = parse(pairs);
   while (to_visit.length > 0) {
     const current = to_visit.shift();
-    if (current === target) return true;
+    if (graph[current] && graph[current].includes(target)) return true;
     visited.add(current);
-    const children = getChildren(pairs, current);
+    const children = graph[current] != undefined ? graph[current] : [];
     for (const child of children) if (!visited.has(child)) to_visit.push(child);
   }
   return false;
