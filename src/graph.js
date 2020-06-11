17d9828140a8435fa35fb5a6f1aa4701252ebd5b
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-06-11 12:29:37 +0530

    Updated bfs function
    created class graph and queue

--- src/graph.js
+++ src/graph.js
@@ -2,26 +2,66 @@ const getChildren = function (pairs, [source]) {
   return pairs.filter(([from]) => from === source).map(([, to]) => to);
 };
 
-const parse = function (pairs) {
-  return pairs.reduce((graph, pair) => {
-    const [from] = pair;
-    if (graph[from] == undefined) {
-      graph[from] = getChildren(pairs, pair);
-    }
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(element) {
+    this.queue.push(element);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  isEmpty() {
+    return !Boolean(this.queue.length);
+  }
+}
+
+class Graph {
+  constructor() {
+    this.connections = {};
+  }
+
+  areNodeConnected(source, dest) {
+    const sourceConnections = this.connections[source];
+    if (sourceConnections) return sourceConnections.includes(dest);
+    return false;
+  }
+
+  getChildren(node) {
+    const nodeConnections = this.connections[node];
+    if (nodeConnections) return nodeConnections;
+    return [];
+  }
+
+  static init(pairs) {
+    const graph = new Graph();
+    graph.connections = pairs.reduce((graph, pair) => {
+      const [from] = pair;
+      if (graph[from] == undefined) graph[from] = getChildren(pairs, pair);
+      return graph;
+    }, {});
     return graph;
-  }, {});
-};
+  }
+}
 
 const bfs = function (pairs, source, target) {
   const visited = new Set();
-  const to_visit = [source];
-  const graph = parse(pairs);
-  while (to_visit.length > 0) {
-    const current = to_visit.shift();
-    if (graph[current] && graph[current].includes(target)) return true;
+  const to_visit = new Queue();
+  to_visit.enqueue(source);
+  const graph = Graph.init(pairs);
+
+  while (!to_visit.isEmpty()) {
+    const current = to_visit.dequeue();
+    if (graph.areNodeConnected(current, target)) return true;
     visited.add(current);
-    const children = graph[current] != undefined ? graph[current] : [];
-    for (const child of children) if (!visited.has(child)) to_visit.push(child);
+    
+    graph.getChildren(current).forEach((child) => {
+      if (!visited.has(child)) to_visit.enqueue(child);
+    });
   }
   return false;
 };
